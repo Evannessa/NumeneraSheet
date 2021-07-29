43ca2008beaa2667ca67a476d606881562786a08
Eva <EvanesceExotica@gmail.com>
2021-07-29 15:34:44 -0400

    few more little changes

--- scripts/NumeneraSheet.js
+++ scripts/NumeneraSheet.js
@@ -1,8 +1,10 @@
 // import {
-	// CypherSystem
+// CypherSystem
 // } from "../../../systems/cyphersystem/module/cyphersystem.js";
 // import CypherSystemActorSheet, { CypherActorSheet } from "../../../systems/cyphersystem/module/actor/actor-sheet.js";
-import {CypherActorSheetPC} from "../../../systems/cyphersystem/module/actor/pc-sheet.js";
+import {
+	CypherActorSheetPC
+} from "../../../systems/cyphersystem/module/actor/pc-sheet.js";
 // import NumeneraSheet from "templates\NumeneraSheet.html"
 export class NumeneraSheet extends CypherActorSheetPC {
 	get template() {
@@ -22,7 +24,7 @@ export class NumeneraSheet extends CypherActorSheetPC {
 		//       });
 	}
 	getData() {
-	const data = super.getData();
+		const data = super.getData();
 		return data;
 	}
 	_createEditor(target, editorOptions, initialContent) {
@@ -34,73 +36,107 @@ export class NumeneraSheet extends CypherActorSheetPC {
 	async _onEditorSave(target, element, content) {
 		return this.submit();
 	}
-	activateListeners(html){
+	activateListeners(html) {
 		super.activateListeners(html);
 
 		//full rest button click
 		html.find(".rest").click(clickEvent => {
 			this.actor.update({
-				"data.pools.might.value":this.actor.data.data.pools.might.max,
-				"data.pools.speed.value":this.actor.data.data.pools.speed.max,
-				"data.pools.intellect.value":this.actor.data.data.pools.intellect.max,
+				"data.pools.might.value": this.actor.data.data.pools.might.max,
+				"data.pools.speed.value": this.actor.data.data.pools.speed.max,
+				"data.pools.intellect.value": this.actor.data.data.pools.intellect.max,
 			})
 		})
+		if (this.actor.getFlag("world", "Advancement") == null) {
+			SaveAdvancements(this.actor, html)
+		}
+
+		let expandButtons = Array.from(html.find(".expand-toggle"));
+		for (let expandButton of expandButtons) {
+			expandButton.addEventListener("click", (event) => toggleExpandClass(event, this.actor))
+		}
+		let cards = Array.from(html.find(".card"))
+		for (let card of cards) {
+			if (card.querySelector(".expand-toggle") != null) {
+				//if this isn't set yet, set it to false as default
+				if (this.actor.getFlag("world", card.id) == null) {
+					this.actor.setFlag("world", card.id, false);
+				}
+				if (this.actor.getFlag("world", card.id) == true) {
+					preExpand(card)
+				} else {
+					let panel = card.querySelector(".panel")
+					panel.prepend(returnOverlayHTML())
+				}
+
+			}
+		}
+		let recoveryButton = Array.from(html.find("#recovery"))[0]
+		recoveryButton.addEventListener("click", () => game.cyphersystem.recoveryRollMacro(this.actor))
+
+		populateTiersFromFlags(this.actor, html)
+		let advancementTiers = Array.from(html.find(".tier"));
+		for (let advancementTier of advancementTiers) {
+			advancementTier.addEventListener("change", () => {
+				SaveAdvancements(this.actor, html);
+			})
+		}
+		desaturateRankingColors(html)
 
 	}
 
 }
 
 Hooks.on('renderActorSheet', (app, html, data) => {
-	// data.actor.unsetFlag("world", "Advancement")
-	console.log("SHEET RENDERED")
-	// CreateFlagForAdvancements(data.actor)
-	
-	// saveIncentivesAndObjections(data.actor, html)
 
-	if(data.actor.getFlag("world", "Advancement") == null){
-		console.log("Creating flags")
-		SaveAdvancements(data.actor, html)
-	}
+	// // if(data.actor.getFlag("world", "scrollX") == null){
+	// // 	data.actor.setFlag("world", "scrollX", 0)
+	// // }
+	// // else{
+	// // 	html.find(".stats-section")[0].closest(".window-content").scrollTop = data.actor.getFlag("world", "scrollX")
+	// // }
+	// if(data.actor.getFlag("world", "Advancement") == null){
+	// 	SaveAdvancements(data.actor, html)
+	// }
 
-	let expandButtons = Array.from(html.find(".expand-toggle"));
-	for(let expandButton of expandButtons){
-		expandButton.addEventListener("click", (event)=> toggleExpandClass(event, data.actor))
-	}
-	let cards = Array.from(html.find(".card"))
-	for(let card of cards){
-		if(card.querySelector(".expand-toggle") !=null){
-			//if this isn't set yet, set it to false as default
-			if(data.actor.getFlag("world", card.id) == null){
-				data.actor.setFlag("world", card.id, false);
-			}
-			if(data.actor.getFlag("world", card.id) == true){
-				preExpand(card)
-			}
-			else{
-				let panel = card.querySelector(".panel")
-				panel.prepend(returnOverlayHTML())
-			}
+	// let expandButtons = Array.from(html.find(".expand-toggle"));
+	// for(let expandButton of expandButtons){
+	// 	expandButton.addEventListener("click", (event)=> toggleExpandClass(event, data.actor))
+	// }
+	// let cards = Array.from(html.find(".card"))
+	// for(let card of cards){
+	// 	if(card.querySelector(".expand-toggle") !=null){
+	// 		//if this isn't set yet, set it to false as default
+	// 		if(data.actor.getFlag("world", card.id) == null){
+	// 			data.actor.setFlag("world", card.id, false);
+	// 		}
+	// 		if(data.actor.getFlag("world", card.id) == true){
+	// 			preExpand(card)
+	// 		}
+	// 		else{
+	// 			let panel = card.querySelector(".panel")
+	// 			panel.prepend(returnOverlayHTML())
+	// 		}
 
-		}
-	}
-	let recoveryButton = Array.from(html.find("#recovery"))[0]
-	recoveryButton.addEventListener("click", ()=> game.cyphersystem.recoveryRollMacro(data.actor))
+	// 	}
+	// }
+	// let recoveryButton = Array.from(html.find("#recovery"))[0]
+	// recoveryButton.addEventListener("click", ()=> game.cyphersystem.recoveryRollMacro(data.actor))
 
-	populateTiersFromFlags(data.actor, html)
-	let advancementTiers = Array.from(html.find(".tier"));
-	for(let advancementTier of advancementTiers){
-		advancementTier.addEventListener("change", ()=>{
-			SaveAdvancements(data.actor, html);
-		})
-	}
+	// populateTiersFromFlags(data.actor, html)
+	// let advancementTiers = Array.from(html.find(".tier"));
+	// for(let advancementTier of advancementTiers){
+	// 	advancementTier.addEventListener("change", ()=>{
+	// 		SaveAdvancements(data.actor, html);
+	// 	})
+	// }
 });
 
-function preExpand(card){
+function preExpand(card) {
 	var button = card.querySelector(".expand-toggle")
-	if(button.firstChild.className == "fas fa-chevron-circle-down"){
+	if (button.firstChild.className == "fas fa-chevron-circle-down") {
 		button.firstChild.className = "fas fa-chevron-circle-up";
-	}
-	else{
+	} else {
 		button.firstChild.className = "fas fa-chevron-circle-down";
 	}
 	//get the card parent, add expanded class
@@ -108,7 +144,7 @@ function preExpand(card){
 	card.classList.add("expanded")
 	//if this card has a wrapper, add the wrapper too
 	let wrapper = card.closest(".card__wrapper")
-	if(wrapper != null){
+	if (wrapper != null) {
 		wrapper.classList.add("expanded")
 	}
 
@@ -117,13 +153,16 @@ function preExpand(card){
 	// panel.prepend(returnOverlayHTML())
 }
 
-function toggleExpandClass(event, actor){
+function toggleExpandClass(event, actor) {
 	var button = event.currentTarget;
+
+	//save scroll position
+	// let scrollTop = button.closest(".window-content").scrollTop
+	// saveScrollPosition(actor, scrollTop)
 	//switch the icon
-	if(button.firstChild.className == "fas fa-chevron-circle-down"){
+	if (button.firstChild.className == "fas fa-chevron-circle-down") {
 		button.firstChild.className = "fas fa-chevron-circle-up";
-	}
-	else{
+	} else {
 		button.firstChild.className = "fas fa-chevron-circle-down";
 	}
 
@@ -135,7 +174,7 @@ function toggleExpandClass(event, actor){
 
 	//if this card has a wrapper, add the wrapper too
 	let wrapper = cardParent.closest(".card__wrapper")
-	if(wrapper != null){
+	if (wrapper != null) {
 		wrapper.classList.add("expanded")
 	}
 	//set a boolean value for if the classlist is expanded
@@ -147,17 +186,14 @@ function toggleExpandClass(event, actor){
 	//get the, add overlay
 	var panel = cardParent.querySelector(".panel")
 	var overlay = panel.querySelector(".panel-overlay")
-	console.log(panel)
-	console.log(overlay)
-	if(cardParent.classList.contains("expanded")){
+	if (cardParent.classList.contains("expanded")) {
 		panel.removeChild(overlay);
-	}
-	else{
+	} else {
 		panel.prepend(returnOverlayHTML())
 	}
 }
 
-function returnOverlayHTML(){
+function returnOverlayHTML() {
 	const overlay = document.createElement('div');
 	overlay.className = "panel-overlay"
 	return overlay
@@ -167,28 +203,50 @@ function returnOverlayHTML(){
 
 
 async function CreateFlagForAdvancements(actor) {
-	for(let tier of tiers){
+	for (let tier of tiers) {
 		let inputs = tier.querySelectorAll('input[type=text]')
 		var i = 0;
-		for(let input of inputs){
+		for (let input of inputs) {
 			input.value = advancementArray[i].text
 			i++;
 		}
 
 	}
-    let flagSet = await actor.setFlag("world", "Advancement", [])
+	let flagSet = await actor.setFlag("world", "Advancement", [])
 
 }
-function populateTiersFromFlags(actor, thisHTML){
+function desaturateRankingColors(html){
+	var rankings = Array.from(html.find(".item-quantity"))
+	for(var ranking of rankings){
+		if(ranking.style.color == "grey"){
+			console.log("ranking is archived")
+			continue;
+		}
+		switch(ranking.innerText){
+			case "Trained":
+				ranking.style.color = "#00A36C"	//"#2a9d8f"//"#00A36C"	
+				break;
+			case "Specialized":
+				ranking.style.color = "#1C80AB"//277DA1"// 577590"//#6495ED"
+				break;
+			case "Inability":
+				ranking.style.color = "#E76F51"//#9A2A2A"
+				break
+		}
+	}
+
+}
+
+function populateTiersFromFlags(actor, thisHTML) {
 	let tiers = Array.from(thisHTML.find(".tier"));
 	let advancements = actor.getFlag("world", "Advancement");
 	let tierIndex = 0;
-	for(let tier of tiers){
+	for (let tier of tiers) {
 		let advancementArray = advancements[tierIndex][`${tier.id}`];
 		tierIndex++;
 		let inputs = tier.querySelectorAll('input[type=text]')
 		var i = 0;
-		for(let input of inputs){
+		for (let input of inputs) {
 			input.value = advancementArray[i].text
 			i++;
 		}
@@ -196,15 +254,20 @@ function populateTiersFromFlags(actor, thisHTML){
 	}
 
 }
+
 function SaveAdvancements(actor, thisHTML) {
-    //place the arrays into the flag to save them to be reopened next time
+	//save the scroll position
+	// let scrollTop = thisHTML.find(".numeneraoverhaul")[0].closest(".window-content").scrollTop
+	// saveScrollPosition(actor, scrollTop)
+
+	//place the arrays into the flag to save them to be reopened next time
 	let storedTiers = []
 
 	let tiers = Array.from(thisHTML.find(".tier"));
-	for(let tier of tiers){
+	for (let tier of tiers) {
 		let inputAdvancements = tier.querySelectorAll('input[type=text]')
 		var storedInputs = []
-		for(let input of inputAdvancements){
+		for (let input of inputAdvancements) {
 			var newStoredAdvancement = {
 				"text": input.value
 			}
@@ -217,21 +280,29 @@ function SaveAdvancements(actor, thisHTML) {
 		storedTiers.push(tierObject) //push the object to storedTiers
 	}
 
-    actor.setFlag("world", "Advancement", storedTiers)
-    //clear incentives and objections array  so they are fresh on restart
+	actor.setFlag("world", "Advancement", storedTiers)
+	//clear incentives and objections array  so they are fresh on restart
 
 }
 
-Hooks.once("init", function(){
+function saveScrollPosition(actor, scrollTop) {
+	actor.setFlag("world", "scrollX", scrollTop)
+}
+
+function getScrollPosition(actor) {
+	return actor.getFlag("world", "scrollX")
+}
+
+Hooks.once("init", function () {
 	loadHandleBarTemplates();
 });
-async function loadHandleBarTemplates(){
+async function loadHandleBarTemplates() {
 	const templatePaths = [
 		"modules/NumeneraSheet/templates/LastingDamage.html"
 	];
 	return loadTemplates(templatePaths);
 }
 Actors.registerSheet("cypher", NumeneraSheet, {
-	types:["PC"],
+	types: ["PC"],
 	makeDefault: true
 });
